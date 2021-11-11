aaa7e2cbe6aca49f779678e0ff6adc85b79dd608
lugonthier <lucas.gonthier@icloud.com>
2021-11-10 23:52:33 -0500

    add autolog, training size param. try to generalize main

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -10,16 +10,17 @@ class Experiment:
             
     """
 
-    def __init__(self, name, model,  tracking_uri="http://localhost:5000", experiment_id=None):
-        self.name = name #Must be unique
-        self.model = model
-         
+    def __init__(self, model=None, experiment_name="",  tracking_uri="http://localhost:5000", experiment_id=None):
+        self.name = experiment_name #Must be unique
+        self.model = model #whole pipeline to prevent data leakage during cross validation 
         self.tracking_uri = tracking_uri
+        
         
         if not experiment_id:
             self.experiment_id = mlflow.create_experiment(name=self.name)
             
         else:
+            
             self.experiment_id = experiment_id
             
 
@@ -30,40 +31,57 @@ class Experiment:
         
         mlflow.set_tracking_uri(self.tracking_uri)
         
-        with mlflow.start_run(experiment_id=self.experiment_id):
+        with mlflow.start_run(experiment_id=self.experiment_id) as run:
             scores = cross_validate(self.model, X=X, y=y, scoring=scorers, cv=cv, return_train_score=return_train_score)
             
-            params = self.model.get_params(deep=False)
             
+            #self.__save_params()
+            params = self.model.get_params(deep=True)
+
             for param, value in params.items():
                 mlflow.log_param(param, value)
+            
+            mlflow.log_param("training size", len(X)/cv)
 
             for metric, score in scores.items():  
                 mlflow.log_metric(metric + '_mean', np.mean(score))
                 mlflow.log_metric(metric + '_std', np.std(score))
+        
+        
 
-
-    def run_simple_experimentation(self, X_train, y_train,  X_test, y_test, metrics):
+        
+    def run_simple_experimentation(self, X_train, y_train,  X_test, y_test, prefix="valid", metrics=None):
         """
         TODO : - TEST if cross_val == False then X_test and y_test not None
                 - If EXCEPTION delete experiment
         """
 
         mlflow.set_tracking_uri(self.tracking_uri)
-        
+        mlflow.sklearn.autolog(log_models=False)
         with mlflow.start_run(experiment_id=self.experiment_id):
             
             self.model.fit(X_train, y_train)
             
-            y_pred = self.model.predict(X_test)
+            #y_pred = self.model.predict(X_test)
 
-            params = self.model.get_params(deep=False)
+            #params = self.model[-1].get_params(deep=False)
 
+            #for param, value in params.items():
+            #    mlflow.log_param(param, value)
+            mlflow.log_param("training size", len(X_train))
+            mlflow.sklearn.eval_and_log_metrics(self.model, X_test, y_test, prefix=prefix)   
+            #for key, metric in metrics.items():  
+            #    mlflow.log_metric(key, metric(y_pred, y_test))
+        
+
+    def __save_params(self):
+        pipeline_params = {} 
+        for step in self.model:
+            pipeline_params[type(step).__name__] = step.get_params(deep=False)
+
+        for step, params in pipeline_params.items():
             for param, value in params.items():
-                mlflow.log_param(param, value)
-                
-            for key, metric in metrics.items():  
-                mlflow.log_metric(key, metric(y_pred, y_test))
+                mlflow.log_param(step+'__'+param, value)
 
                 
         
--- experiment/main.py
+++ experiment/main.py
@@ -1,41 +1,77 @@
 import pandas as pd
-
-
-
+import sys
 from sklearn.model_selection import train_test_split
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, make_scorer
 from experiment import Experiment
 
 
 
-scorer = True
 
 
-df = pd.read_csv('/Users/gonthierlucas/Desktop/DS_project/IMDB_reviews/data/IMDB_Dataset.csv')
 
-X_train, X_test, y_train, y_test = train_test_split(df.review.to_numpy(), df.sentiment.apply(lambda x: 0 if (x == 'negative') else 1))
+def main():
+    if len(sys.argv) < 5:
+        usage = "\n Usage: python classifieur.py method nb_train nb_test lambda bruit corruption don_ab\
+        \n\n\t mode : 1 => simple experiment, 2 => cross validation\
+        \n\t model_seleted : 1 => Logistic Regression, 2 => ... until 6, 7 => Performe all model ?\
+        \n\t new_experiment : 0 => experiment exist, 1: new experiment\
+        \n\t experiment_name (if new_experiment==1)\
+        \n\t experiment_id (if new_experiment==0)\
+        \n\t range_step : step to iterate over dataset size"
+        print(usage)
+        return
 
+    mode = int(sys.argv[1])
+    model_selected = int(sys.argv[2])
+    new_experiment = int(sys.argv[3]) # 0:no, 1:yes
 
-cv = CountVectorizer()
+    if(new_experiment == 1):
+        experiment_name = str(sys.argv[4])
+    else:
+        experiment_id = int(sys.argv[4])
 
-X_train_cv = cv.fit_transform(X_train)
-X_test_cv = cv.transform(X_test)
+    range_step = int(sys.argv[5])
     
 
-model = LogisticRegression()
+    models = {1:LogisticRegression()}
+    if (model_selected > 0) and (model_selected < 7):
+        model = models[model_selected]
+    
+    df = pd.read_csv('/Users/gonthierlucas/Desktop/DS_project/IMDB_reviews/data/IMDB_Dataset.csv')
 
-if scorer:
-    metrics =  {accuracy_score.__name__:make_scorer(accuracy_score), f1_score.__name__:make_scorer(f1_score),
-     roc_auc_score.__name__:make_scorer((roc_auc_score))}
-else :
-    metrics = {accuracy_score.__name__:accuracy_score, f1_score.__name__:f1_score, roc_auc_score.__name__:roc_auc_score}
+    if new_experiment == 1:
+            exp = Experiment( experiment_name=experiment_name)
+            
+    else:
+        exp = Experiment( experiment_id=experiment_id)
 
-exp = Experiment('logisticRegression_cross', model)
+    for size in range(range_step, len(df), range_step):
 
-exp.run_cross_valid_experimentation(X_train_cv, y_train,scorers=metrics, return_train_score=True)
+        X = df.review.to_numpy()[:size]
+        y = df.sentiment.apply(lambda x: 0 if (x == 'negative') else 1).to_numpy()[:size]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-  
+        cv = CountVectorizer()
+
+        pipe = Pipeline(steps=[('vect', cv), ('model', model)])
+
+        exp.model = pipe
+        
+
+        if mode == 1:
+            metrics = {accuracy_score.__name__:accuracy_score, f1_score.__name__:f1_score, roc_auc_score.__name__:roc_auc_score}
+            exp.run_simple_experimentation(X_train, y_train, X_test, y_test, "test_",  metrics)
+
+        elif mode == 2:
+            metrics =  {accuracy_score.__name__:make_scorer(accuracy_score), f1_score.__name__:make_scorer(f1_score),
+            roc_auc_score.__name__:make_scorer((roc_auc_score))}
+
+            exp.run_cross_valid_experimentation(X_train, y_train, scorers=metrics, return_train_score=True)
+
+
+    
+if __name__=="__main__":
+    main()
